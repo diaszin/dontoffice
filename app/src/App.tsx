42ddb20a1289d4a186274397020d0539754e3919
diaszin <84644190+diaszin@users.noreply.github.com>
2026-06-27 19:32:57 -0300

    Altera titulo de teste do deploy

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return <>
-  <h1 className=" text-2xl text-amber-400">Olá mundo com o novo deploy !</h1></>;
+  <h1 className=" text-2xl text-amber-400">Olá mundo com o novo deploy e configuração somente para a pasta app!</h1></>;
 }
 
 export default App;
