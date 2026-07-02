f2a8000a69303a555f7a7a336d31aec26efcaebd
diaszin <84644190+diaszin@users.noreply.github.com>
2026-07-01 23:01:19 -0300

    Add campo de texto slug a página create + Add slogan

--- app/src/pages/ppt/create.tsx
+++ app/src/pages/ppt/create.tsx
@@ -1,19 +1,23 @@
 import DefaultDontofficeLogo from "../../components/shared/DefaultDontofficeLogo";
+import SlugInput from "../../components/shared/SlugInput";
 
 export function PPTCreatePage() {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <div className="flex flex-col items-center justify-center gap-8">  
+      <div className="flex flex-col items-center justify-center gap-8">
         <DefaultDontofficeLogo
           name=".ppt"
           initialGradientColor="#C43E1C"
           finalGradientColor="#FF8C69"
           className="text-[32px] ds-display"
         />
-        <span className="ds-subtitle">Sua próxima apresentação, simples assim.</span>
-      </div>
-      <div className="flex flex-col items-center justify-center">
-        
+        <span className="ds-subtitle">
+          Sua próxima apresentação, simples assim.
+        </span>
+        <div className="flex flex-col justify-center w-full gap-1">
+          <SlugInput placeholder="apresentacao-rapida" />
+          <legend className="ds-legend text-gray-700">Esse é o nome que irá aparecer no seu link</legend>
+        </div>
       </div>
     </div>
   );
