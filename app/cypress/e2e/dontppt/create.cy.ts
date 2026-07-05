304b954e0e9645f8d05eb963f60c70aa770e7d69
diaszin <84644190+diaszin@users.noreply.github.com>
2026-07-05 20:52:28 -0300

    Add base url para os testes no frontend + remove testes desnecessários

---
+++ app/cypress/e2e/dontppt/create.cy.ts
@@ -0,0 +1,7 @@
+/// <reference types="cypress" />
+
+describe('Página de upload do módulo PPT', () => {
+    it("Visita página principal", () =>{
+        cy.visit("http://localhost:5173/ppt")
+    })
+})
