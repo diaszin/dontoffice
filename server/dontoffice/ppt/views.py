6a1bd53b85a107b048259168869a2f860716b06f
diaszin <84644190+diaszin@users.noreply.github.com>
2026-07-18 17:54:15 -0300

    remove importações desnecessárias

--- server/dontoffice/ppt/views.py
+++ server/dontoffice/ppt/views.py
@@ -2,10 +2,10 @@ from django.contrib.auth.models import Group, User
 from rest_framework import permissions, views, viewsets
 from rest_framework.response import Response
 
-from dontoffice.ppt.models import PPTRoute
-from dontoffice.ppt.serializers import GroupSerializer, RouteSerializer, UserSerializer
+import dontoffice.ppt.models
+from dontoffice.ppt.serializers import RouteSerializer
 
 
 class RouteViewSet(viewsets.ModelViewSet):
-    queryset = PPTRoute.objects.all()
+    queryset = dontoffice.ppt.models.PPTRoute.objects.all()
     serializer_class = RouteSerializer
