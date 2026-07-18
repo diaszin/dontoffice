8a751d37c54ffda41a6dcd652fef705f0008987b
diaszin <84644190+diaszin@users.noreply.github.com>
2026-07-18 18:09:20 -0300

    Configura pasta de upload para ser ignorado pelo git + disponibiliza arquivo upado para ser baixado pelo servidor

--- server/dontoffice/urls.py
+++ server/dontoffice/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from django.conf.urls.static import static
+from django.conf import settings
 
 from dontoffice.ppt import views as ppt_views
 
@@ -25,8 +27,15 @@ router = routers.DefaultRouter()
 
 router.register(r"ppt/route", ppt_views.RouteViewSet)
 
+
 urlpatterns = [
     path("", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(prefix=settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+    
+    
