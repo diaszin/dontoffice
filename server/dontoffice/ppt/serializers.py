25329afa1e4715700cf57b4d3dde56cdf5213b19
diaszin <84644190+diaszin@users.noreply.github.com>
2026-07-18 17:52:42 -0300

    Remove serializer desnecessário

--- server/dontoffice/ppt/serializers.py
+++ server/dontoffice/ppt/serializers.py
@@ -4,18 +4,6 @@ from rest_framework import serializers
 from dontoffice.ppt.models import PPTRoute
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ["url", "username", "email", "groups"]
-
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ["url", "name"]
-
-
 class RouteSerializer(serializers.ModelSerializer):
     class Meta:
         model = PPTRoute
